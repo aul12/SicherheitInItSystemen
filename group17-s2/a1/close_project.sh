3fd287927c7f2c05a4940f015b199ca61dd54ae0
Paul Nykiel <paul.nykiel@uni-ulm.de>
2019-12-17 14:14:48 +0100

    Mehr anmerkungen

--- group17-s2/a1/close_project.sh
+++ group17-s2/a1/close_project.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 if [ -n "$1" ]
 then
-    chmod 0750 projects/$1 # Only read access for the project group, the owner can still do everything
+    chmod 0750 projects/$1 # Only read and execute permissions for the project group, the owner can still do everything
 else
     echo "Please specify a project name"
 fi
