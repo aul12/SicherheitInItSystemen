fbd724594c2b7a059910455feaea4aaf46abc1de
gecgab <gecgab@hotmail.com>
2019-12-19 21:33:10 +0100

    a4 complete

--- a4/finddep.sh
+++ a4/finddep.sh
@@ -2,10 +2,10 @@
 
 if [ -n "$1" ]
 then
-	cd ./projects
+	cd ./projects/
 	sha_string=$(echo $1 | sha1sum | awk '{print $1}')	# creates sha string that has to be matched
 	for d in * ; do 		# iterate through all project folders (assuming there are only project folders in ./projects)
-	    	dep_string=.$(cat ./$d/dependencies)
+	    	dep_string=.$(cat $d/dependencies)
 		if [[ $dep_string == *$sha_string* ]]; then
   			echo $d
 		fi
